66d102bff1589f143e43e1b33724100b93b026a7
alisitsky <alisitsky@gmail.com>
2024-07-10 13:48:57 +0200

    Droot task

---
+++ src/main/java/com/alisitsky/tasks/kyu6/BitCounting.java
@@ -0,0 +1,11 @@
+package com.alisitsky.tasks.kyu6;
+
+public class BitCounting {
+
+    public static int countBits(int n){
+        return (int) Integer.toBinaryString(n)
+                .chars()
+                .filter(ch -> ch == '1')
+                .count();
+    }
+}
